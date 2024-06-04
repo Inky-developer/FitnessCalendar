3d8ae45e8564a7decb01d1e929850daf60bdb624
Inky-developer <inky@mailbox.org>
2024-06-04 22:35:12 +0200

    Fix home page stats not updating after creating a new activity
    The problem is that the filters included end dates of when the view models were creates, which meant that new activities would get filtered out. As fix remove the capability of filtering on days in the past for now and set end to null

--- app/src/main/java/com/inky/fitnesscalendar/data/DateRange.kt
+++ app/src/main/java/com/inky/fitnesscalendar/data/DateRange.kt
@@ -7,29 +7,33 @@ import java.time.ZoneId
 import java.time.temporal.ChronoUnit
 import java.util.Date
 
-data class DateRange(val start: Date, val end: Date) {
+data class DateRange(val start: Date, val end: Date?) {
     companion object {
         // Specifies by how many hours offset a day starts.
         // E.g. A value of 2 means that the day goes from 2 am to 2 am next day
         private const val DAY_START_OFFSET_HOURS = 2L
 
         // Let days start at 2 am
-        fun atDay(offsetDays: Long, instant: Instant = Instant.now()): DateRange {
-            val day = LocalDateTime.ofInstant(instant, ZoneId.systemDefault()).plusDays(offsetDays)
-                .minusHours(DAY_START_OFFSET_HOURS)
+        fun atDay(offsetDays: Long): DateRange {
+            val day =
+                LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()).plusDays(offsetDays)
+                    .minusHours(DAY_START_OFFSET_HOURS)
             val startOfToday = day.with(LocalTime.MIN).plusHours(DAY_START_OFFSET_HOURS)
             val endOfToday = day.with(LocalTime.MAX).plusHours(DAY_START_OFFSET_HOURS)
 
             return DateRange(
                 start = Date.from(startOfToday.atZone(ZoneId.systemDefault()).toInstant()),
-                end = Date.from(endOfToday.atZone(ZoneId.systemDefault()).toInstant())
+                end = if (offsetDays == 0L) null else Date.from(
+                    endOfToday.atZone(ZoneId.systemDefault()).toInstant()
+                )
             )
         }
 
-        fun lastDays(numDays: Int, instant: Instant = Instant.now()): DateRange {
+        fun lastDays(numDays: Int): DateRange {
+            val instant = Instant.now()
             return DateRange(
                 start = Date.from(Instant.ofEpochMilli(instant.toEpochMilli() - ChronoUnit.DAYS.duration.toMillis() * numDays)),
-                end = Date.from(instant)
+                end = null
             )
         }
 
