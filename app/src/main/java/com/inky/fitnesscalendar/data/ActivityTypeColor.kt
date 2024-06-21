70c64089457b37581cbc0c4ff115b82f4e37c502
Inky-developer <inky@mailbox.org>
2024-06-21 16:48:12 +0200

    Use nicer colors for activity types

--- app/src/main/java/com/inky/fitnesscalendar/data/ActivityTypeColor.kt
+++ app/src/main/java/com/inky/fitnesscalendar/data/ActivityTypeColor.kt
@@ -6,10 +6,10 @@ import com.inky.fitnesscalendar.R
 
 // TODO: Specify color names
 enum class ActivityTypeColor(@ColorRes val colorId: Int, @StringRes val nameId: Int) {
-    Color1(R.color.stats_1, R.string.color),
-    Color2(R.color.stats_2, R.string.color),
-    Color3(R.color.stats_3, R.string.color),
-    Color4(R.color.stats_4, R.string.color),
-    Color5(R.color.stats_5, R.string.color),
-    ColorOther(R.color.stats_other, R.string.color),
+    Color1(R.color.stats_1, R.string.color_orange),
+    Color2(R.color.stats_2, R.string.color_green),
+    Color3(R.color.stats_3, R.string.color_turquoise),
+    Color4(R.color.stats_4, R.string.color_brown),
+    Color5(R.color.stats_5, R.string.color_yellow),
+    ColorOther(R.color.stats_other, R.string.color_grey),
 }
