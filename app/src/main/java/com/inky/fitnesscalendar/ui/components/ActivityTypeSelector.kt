89c60cacb3d9bb4816d738a98cf9a6262ed5e040
Inky-developer <inky@mailbox.org>
2024-06-21 14:03:18 +0200

    Make activity type & category selector more colorful

--- app/src/main/java/com/inky/fitnesscalendar/ui/components/ActivityTypeSelector.kt
+++ app/src/main/java/com/inky/fitnesscalendar/ui/components/ActivityTypeSelector.kt
@@ -5,10 +5,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import com.inky.fitnesscalendar.data.ActivityType
 
@@ -32,6 +34,11 @@ fun ActivityTypeSelector(
                                 style = MaterialTheme.typography.headlineMedium
                             )
                         },
+                        border = FilterChipDefaults.filterChipBorder(
+                            enabled = true,
+                            selected = isSelected(activityType),
+                            borderColor = colorResource(activityType.colorId)
+                        ),
                         modifier = Modifier.padding(horizontal = 4.dp)
                     )
                 }
