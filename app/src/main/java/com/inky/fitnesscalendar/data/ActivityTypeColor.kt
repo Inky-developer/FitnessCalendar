4aa838c1f4a0440db98e26ac388620a129bfba75
Inky-developer <inky@mailbox.org>
2024-06-22 23:29:25 +0200

    Remove outdated todo

--- app/src/main/java/com/inky/fitnesscalendar/data/ActivityTypeColor.kt
+++ app/src/main/java/com/inky/fitnesscalendar/data/ActivityTypeColor.kt
@@ -4,7 +4,6 @@ import androidx.annotation.ColorRes
 import androidx.annotation.StringRes
 import com.inky.fitnesscalendar.R
 
-// TODO: Specify color names
 enum class ActivityTypeColor(@ColorRes val colorId: Int, @StringRes val nameId: Int) {
     Color1(R.color.stats_1, R.string.color_orange),
     Color2(R.color.stats_2, R.string.color_green),
