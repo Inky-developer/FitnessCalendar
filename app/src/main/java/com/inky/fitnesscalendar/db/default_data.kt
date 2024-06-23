4ff303764c54d5e2fb8fd4e62b756f49bd32e3c6
Inky-developer <inky@mailbox.org>
2024-06-23 12:03:34 +0200

    Fix default data

--- app/src/main/java/com/inky/fitnesscalendar/db/default_data.kt
+++ app/src/main/java/com/inky/fitnesscalendar/db/default_data.kt
@@ -1,9 +1,8 @@
 package com.inky.fitnesscalendar.db
 
-import androidx.annotation.ColorRes
 import androidx.sqlite.db.SupportSQLiteDatabase
-import com.inky.fitnesscalendar.R
 import com.inky.fitnesscalendar.data.ActivityCategory
+import com.inky.fitnesscalendar.data.ActivityTypeColor
 
 // TODO: Add test for this
 fun loadDefaultData(db: SupportSQLiteDatabase) {
@@ -11,12 +10,12 @@ fun loadDefaultData(db: SupportSQLiteDatabase) {
     try {
         for (type in DefaultActivityType.entries) {
             db.execSQL(
-                "INSERT INTO ActivityType(activity_category, name, emoji, color_id, has_vehicle, has_duration) VALUES (?, ?, ?, ?, ?, ?)",
+                "INSERT INTO ActivityType(activity_category, name, emoji, color, has_vehicle, has_duration) VALUES (?, ?, ?, ?, ?, ?)",
                 arrayOf(
                     type.activityCategory,
                     type.title,
                     type.emoji,
-                    type.colorId,
+                    type.color,
                     type.hasVehicle,
                     type.hasDuration
                 )
@@ -33,86 +32,86 @@ enum class DefaultActivityType(
     val activityCategory: ActivityCategory,
     val title: String,
     val emoji: String,
-    @ColorRes val colorId: Int,
+    val color: ActivityTypeColor,
     val hasVehicle: Boolean = false,
     val hasDuration: Boolean = true,
 ) {
     Bouldering(
         ActivityCategory.Sports,
         title = "Bouldering",
-        colorId = R.color.stats_1,
+        color = ActivityTypeColor.Color1,
         emoji = "🧗"
     ),
     Cycling(
         ActivityCategory.Sports,
         title = "Cycling",
-        colorId = R.color.stats_2,
+        color = ActivityTypeColor.Color2,
         emoji = "🚴"
     ),
     Running(
         ActivityCategory.Sports,
         title = "Running",
-        colorId = R.color.stats_3,
+        color = ActivityTypeColor.Color3,
         emoji = "🏃"
     ),
     KungFu(
         ActivityCategory.Sports,
         title = "Kung Fu",
-        colorId = R.color.stats_4,
+        color = ActivityTypeColor.Color4,
         emoji = "🥋"
     ),
     UniversityCommute(
         ActivityCategory.Travel,
         title = "Home → Uni",
-        colorId = R.color.stats_1,
+        color = ActivityTypeColor.Color1,
         emoji = "🏢",
         hasVehicle = true
     ),
     HomeCommute(
         ActivityCategory.Travel,
         title = "Uni → Home",
-        colorId = R.color.stats_2,
+        color = ActivityTypeColor.Color2,
         emoji = "🏡",
         hasVehicle = true
     ),
     Travel(
         ActivityCategory.Travel,
         title = "Travel",
-        colorId = R.color.stats_3,
+        color = ActivityTypeColor.Color3,
         emoji = "🗺️",
         hasVehicle = true
     ),
-    Work(ActivityCategory.Work, title = "Work", colorId = R.color.stats_1, emoji = "💼"),
+    Work(ActivityCategory.Work, title = "Work", color = ActivityTypeColor.Color1, emoji = "💼"),
     Gaming(
         ActivityCategory.Entertainment,
         title = "Gaming",
-        colorId = R.color.stats_1,
+        color = ActivityTypeColor.Color1,
         emoji = "🎮"
     ),
     Film(
         ActivityCategory.Entertainment,
         title = "Film",
-        colorId = R.color.stats_2,
+        color = ActivityTypeColor.Color2,
         emoji = "🎬"
     ),
     Note(
         ActivityCategory.Other,
         title = "Note",
-        colorId = R.color.stats_1,
+        color = ActivityTypeColor.Color1,
         emoji = "📓",
         hasDuration = false
     ),
     HealthNote(
         ActivityCategory.Other,
         title = "Health Note",
-        colorId = R.color.stats_2,
+        color = ActivityTypeColor.Color2,
         emoji = "🩹",
         hasDuration = false
     ),
     Other(
         ActivityCategory.Other,
         title = "Other",
-        colorId = R.color.stats_3,
+        color = ActivityTypeColor.Color3,
         emoji = "🏷️"
     );
 }
