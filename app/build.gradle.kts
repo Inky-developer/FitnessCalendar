66ad0c7c7333cfec8f8ee4cac54f364f9f826edb
Inky-developer <inky@mailbox.org>
2024-06-25 00:02:06 +0200

    Additional app size optimization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ android {
         }
         getByName("release") {
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
