441f7fdb79a9c8e3ddf3c1b9c34d21a98302754f
Inky-developer <developerinky@gmail.com>
2024-08-17 02:21:49 +0200

    Render track in activity card

---
+++ app/src/main/java/com/inky/fitnesscalendar/db/entities/CoordinateRect.kt
@@ -0,0 +1,8 @@
+package com.inky.fitnesscalendar.db.entities
+
+data class CoordinateRect(
+    val latitudeMin: Double,
+    val latitudeMax: Double,
+    val longitudeMin: Double,
+    val longitudeMax: Double
+)
