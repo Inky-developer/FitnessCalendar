6b10c48117da230b751fe324e18f3a92ddb34a2b
Inky-developer <developerinky@gmail.com>
2024-08-24 20:20:40 +0200

    Only apply `safeDrawingPadding` in Landscape mode

--- app/src/main/java/com/inky/fitnesscalendar/MainActivity.kt
+++ app/src/main/java/com/inky/fitnesscalendar/MainActivity.kt
@@ -2,6 +2,7 @@ package com.inky.fitnesscalendar
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
@@ -15,6 +16,7 @@ import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import com.inky.fitnesscalendar.ui.App
 import com.inky.fitnesscalendar.ui.theme.FitnessCalendarTheme
 import com.inky.fitnesscalendar.util.EXTRA_TOAST
@@ -40,12 +42,19 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             FitnessCalendarTheme {
-                // A surface container using the 'background' color from the theme
+                // In landscape mode, use the more conservative `safeDrawingPadding`, which looks
+                // uglier but makes sure that no content is obstructed
+                val orientation = LocalConfiguration.current.orientation
+                val baseModifier = Modifier
+                    .fillMaxSize()
+                    .imePadding()
+                val modifier = when (orientation) {
+                    Configuration.ORIENTATION_LANDSCAPE -> baseModifier.safeDrawingPadding()
+                    else -> baseModifier
+                }
+
                 Surface(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .imePadding()
-                        .safeDrawingPadding(),
+                    modifier = modifier,
                     color = MaterialTheme.colorScheme.background
                 ) {
                     App()
