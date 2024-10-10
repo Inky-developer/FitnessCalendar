b13acd88a96b494a81d2aa47e91091de8977bed4
Inky-developer <developerinky@gmail.com>
2024-10-10 16:07:14 +0200

    Add more content colors

--- app/src/main/java/com/inky/fitnesscalendar/data/ContentColor.kt
+++ app/src/main/java/com/inky/fitnesscalendar/data/ContentColor.kt
@@ -10,5 +10,9 @@ enum class ContentColor(@ColorRes val colorId: Int, @StringRes val nameId: Int) 
     Color3(R.color.content_3, R.string.color_turquoise),
     Color4(R.color.content_4, R.string.color_brown),
     Color5(R.color.content_5, R.string.color_yellow),
+    Color6(R.color.content_6, R.string.color_purple),
+    Color7(R.color.content_7, R.string.color_ice),
+    Color8(R.color.content_8, R.string.color_red),
+    Color9(R.color.content_9, R.string.color_black),
     ColorOther(R.color.content_other, R.string.color_grey),
 }
