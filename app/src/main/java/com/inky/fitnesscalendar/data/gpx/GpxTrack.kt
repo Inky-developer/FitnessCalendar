d9df31103d360d6a55a6f3383eae1521a9dec972
Inky-developer <developerinky@gmail.com>
2024-10-13 23:45:00 +0200

    CI: Fix error

--- app/src/main/java/com/inky/fitnesscalendar/data/gpx/GpxTrack.kt
+++ app/src/main/java/com/inky/fitnesscalendar/data/gpx/GpxTrack.kt
@@ -1,6 +1,13 @@
 package com.inky.fitnesscalendar.data.gpx
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
+
+@Serializable
 data class GpxTrack(val name: String, val type: String, val points: List<GpxTrackPoint>) {
+    @Transient
     val startTime = points.firstOrNull()?.time
+
+    @Transient
     val endTime = points.lastOrNull()?.time
 }
