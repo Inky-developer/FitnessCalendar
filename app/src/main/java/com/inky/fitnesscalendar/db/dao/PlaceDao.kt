bec2e851609d6bfcd4805f8526e118c971edfb6a
Inky-developer <developerinky@gmail.com>
2024-12-01 23:03:30 +0100

    Places: Also order by last used as a tie-breaker

--- app/src/main/java/com/inky/fitnesscalendar/db/dao/PlaceDao.kt
+++ app/src/main/java/com/inky/fitnesscalendar/db/dao/PlaceDao.kt
@@ -11,7 +11,7 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PlaceDao {
-    @Query("SELECT Place.*, COUNT(Activity.place_id) as count FROM Place LEFT JOIN Activity ON Place.uid = Activity.place_id GROUP BY place.uid ORDER BY count DESC")
+    @Query("SELECT Place.*, COUNT(Activity.place_id) as count, MAX(Activity.start_time) AS last_used FROM Place LEFT JOIN Activity ON Place.uid = Activity.place_id GROUP BY place.uid ORDER BY count DESC, last_used DESC  ")
     fun getAll(): Flow<List<Place>>
 
     @Query("SELECT place_id, COUNT(*) AS count FROM Activity WHERE place_id IS NOT NULL GROUP BY place_id")
