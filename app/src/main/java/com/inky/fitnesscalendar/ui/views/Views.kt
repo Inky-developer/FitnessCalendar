55856ba54b0aab697488d380236fe5e1a4a7a2f8
Inky-developer <inky@mailbox.org>
2025-03-29 21:13:29 +0100

    Views: Don't serialize the name id
    it is not required and was an oversight

--- app/src/main/java/com/inky/fitnesscalendar/ui/views/Views.kt
+++ app/src/main/java/com/inky/fitnesscalendar/ui/views/Views.kt
@@ -3,6 +3,7 @@ package com.inky.fitnesscalendar.ui.views
 import android.os.Parcelable
 import com.inky.fitnesscalendar.R
 import com.inky.fitnesscalendar.data.EpochDay
+import com.inky.fitnesscalendar.util.DEEP_LINK_BASE_PATH
 import com.inky.fitnesscalendar.view_model.statistics.Period
 import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
@@ -10,54 +11,82 @@ import kotlinx.serialization.Serializable
 
 @Parcelize
 @Serializable
-sealed class Views(val nameId: Int) : Parcelable {
-    @Serializable
-    data object Home : Views(R.string.today)
+sealed class Views : Parcelable {
+    abstract val nameId: Int
 
     @Serializable
-    data class EditDay(val primitiveEpochDay: Long) : Views(R.string.edit_day) {
+    data object Home : Views() {
+        override val nameId get() = R.string.today
+    }
+
+    @Serializable
+    data class EditDay(val primitiveEpochDay: Long) : Views() {
+        override val nameId get() = R.string.edit_day
+
         val epochDay get() = EpochDay(primitiveEpochDay)
     }
 
     @Serializable
-    data class DayView(val primitiveEpochDay: Long? = null) : Views(R.string.days) {
+    data class DayView(val primitiveEpochDay: Long? = null) : Views() {
+        override val nameId get() = R.string.days
+
         val epochDay
             get() = primitiveEpochDay?.let { EpochDay(primitiveEpochDay) } ?: EpochDay.today()
     }
 
     @Serializable
-    data class ActivityLog(val activityId: Int? = null) : Views(R.string.activity_log)
+    data class ActivityLog(val activityId: Int? = null) : Views() {
+        override val nameId get() = R.string.activity_log
+    }
 
     @Serializable
-    data object FilterActivity : Views(R.string.filter)
+    data object FilterActivity : Views() {
+        override val nameId get() = R.string.filter
+    }
 
     @Serializable
     data class NewActivity(val activityId: Int? = null, val rawInitialStartDay: Long? = null) :
-        Views(R.string.new_activity) {
+        Views() {
+        override val nameId get() = R.string.new_activity
+
         val initialStartDay: EpochDay? get() = rawInitialStartDay?.let { EpochDay(it) }
     }
 
     @Serializable
-    data class TrackDetails(val activityId: Int) : Views(R.string.track_details)
+    data class TrackDetails(val activityId: Int) : Views() {
+        override val nameId get() = R.string.track_details
+    }
 
     @Serializable
     data class TrackGraph(val activityId: Int, val projection: TrackGraphProjection) :
-        Views(R.string.track_graph)
+        Views() {
+        override val nameId get() = R.string.track_graph
+    }
 
     @Serializable
-    data class Map(val activityId: Int) : Views(R.string.Map)
+    data class Map(val activityId: Int) : Views() {
+        override val nameId get() = R.string.Map
+    }
 
     @Serializable
-    data object SummaryView : Views(R.string.summary)
+    data object SummaryView : Views() {
+        override val nameId get() = R.string.summary
+    }
 
     @Serializable
-    data object RecordActivity : Views(R.string.record_activity)
+    data object RecordActivity : Views() {
+        override val nameId get() = R.string.record_activity
+    }
 
     @Serializable
-    data object Settings : Views(R.string.settings)
+    data object Settings : Views() {
+        override val nameId get() = R.string.settings
+    }
 
     @Serializable
-    data class Statistics(val primitiveInitialPeriod: String? = null) : Views(R.string.statistics) {
+    data class Statistics(val primitiveInitialPeriod: String? = null) : Views() {
+        override val nameId get() = R.string.statistics
+
         val initialPeriod get() = primitiveInitialPeriod?.let { Period.valueOf(it) }
     }
 }
