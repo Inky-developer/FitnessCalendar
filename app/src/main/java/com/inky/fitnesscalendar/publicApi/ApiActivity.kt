11ce6e1a9ec8702389ff9251bb5302477d5a3336
Inky-developer <inky@mailbox.org>
2025-04-04 00:10:21 +0200

    Api: Add translations

--- app/src/main/java/com/inky/fitnesscalendar/publicApi/ApiActivity.kt
+++ app/src/main/java/com/inky/fitnesscalendar/publicApi/ApiActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.lifecycle.lifecycleScope
+import com.inky.fitnesscalendar.R
 import com.inky.fitnesscalendar.db.entities.ActivityType
 import com.inky.fitnesscalendar.repository.DatabaseRepository
 import kotlinx.coroutines.Dispatchers
@@ -36,7 +37,10 @@ abstract class ApiActivity : ComponentActivity() {
         val activityTypeName = intent.getStringExtra(EXTRA_ACTIVITY_TYPE)
         if (activityTypeName == null) {
             runOnUiThread {
-                Toast.makeText(this, "No activity type specified", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    this,
+                    getString(R.string.error_no_activity_type_specified), Toast.LENGTH_LONG
+                ).show()
             }
             return null
         }
@@ -44,7 +48,11 @@ abstract class ApiActivity : ComponentActivity() {
         val activityType = activityTypes.find { it.name == activityTypeName }
         if (activityType == null) {
             runOnUiThread {
-                Toast.makeText(this, "Unknown activity type: $activityTypeName", Toast.LENGTH_LONG)
+                Toast.makeText(
+                    this,
+                    getString(R.string.error_unknown_activity_type, activityTypeName),
+                    Toast.LENGTH_LONG
+                )
                     .show()
             }
             return null
