c8bb27821de113e2bc6b8be3420d016db8049fd3
Inky-developer <inky@mailbox.org>
2025-11-26 00:04:30 +0100

    Fix bug when adding multiple videos

--- app/src/main/java/com/inky/fitnesscalendar/ui/components/imageComponents.kt
+++ app/src/main/java/com/inky/fitnesscalendar/ui/components/imageComponents.kt
@@ -260,7 +260,7 @@ private fun ExtractImageFromVideoPopup(
     var loadingFrame by rememberSaveable(videoUri) { mutableStateOf(true) }
     var frameBitmap by rememberSaveable(videoUri) { mutableStateOf<Bitmap?>(null) }
 
-    LaunchedEffect(Unit) {
+    LaunchedEffect(videoUri) {
         snapshotFlow { frameIndex }.debounce(50).collectLatest { frameIndex ->
             loadingFrame = true
             try {
