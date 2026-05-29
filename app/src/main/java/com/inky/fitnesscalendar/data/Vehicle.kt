6e95b2b946258066878ddf9cb2a4b043548aeb3c
Inky-developer <inky@mailbox.org>
2026-05-29 19:14:36 +0200

    Add plane vehicle

--- app/src/main/java/com/inky/fitnesscalendar/data/Vehicle.kt
+++ app/src/main/java/com/inky/fitnesscalendar/data/Vehicle.kt
@@ -9,7 +9,8 @@ enum class Vehicle(@StringRes val nameId: Int, val emoji: String) {
     Train(R.string.vehicle_train, "🚆"),
     Car(R.string.vehicle_car, "🚗"),
     Bike(R.string.vehicle_bike, "🚴"),
-    Foot(R.string.vehicle_walking, "🚶");
+    Foot(R.string.vehicle_walking, "🚶"),
+    Plane(R.string.vehicle_plane, "✈️");
 
     @Composable
     fun text() = stringResource(nameId)
